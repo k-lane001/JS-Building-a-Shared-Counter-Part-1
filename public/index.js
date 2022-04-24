685377eeba8a5f66a8fed756a73a251a111cfb4a
Kaylie Lane <k.lane5612@gmail.com>
2022-04-24 14:11:59 -0400

    Bonus Clear Button

--- public/index.js
+++ public/index.js
@@ -2,6 +2,13 @@ async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+//add new button - to clear counter
+    const clearButton = document.createElement('button');
+    clearButton.style.backgroundColor = 'red'
+    clearButton.style.color = 'white'
+    clearButton.textContent = 'Clear'
+    document.getElementsByTagName('div')[2].appendChild(clearButton);
+
 //asynchronous fetch variable to retrieve stored counter data from local host
     const response = await fetch('http://localhost:9001/counter');
 //asynchronous response variable to parse returned json data
@@ -18,9 +25,20 @@ async function main(){
         countValue--;
         countContainer.textContent = countValue;
     }
+//Clear function
+    function clearResult() {
+        countValue = 0;
+        countContainer.textContent=countValue;
+    }
 
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    //Clear function event listener
+    clearButton.addEventListener('click', clearResult);
     countContainer.textContent = countValue;
+
 }
 main()
+
+
+
